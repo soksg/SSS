7018d5fb7915b8e1290aac6fd91541e1c0760114
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-11-10 07:45:35 +0000

    [Add]routes.rb 記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,41 @@
 Rails.application.routes.draw do
-  devise_for :members ,skip: [:passwords], controllers: {
-    registrations: "public/registrations",
-    sessions: "public/sessions"
-  }
 
-  devise_for :admins ,skip: [:passwords, :registrations] ,controllers: {
-    sessions: "admin/sessions"
-  }
-  
-    namespace :public do
-    get 'posts/index'
-    get 'posts/show'
-    get 'posts/create'
-    get 'posts/edit'
-    get 'posts/update'
-    get 'posts/destroy'
-  end
-  
+  root to: "homes#top"
+  get "/home/about" => "homes#about"
+
+  devise_for :members, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: "public/sessions"
+}
+  devise_for :admins, skip: [:passwords, :registrations] ,controllers: {
+  sessions: "admin/sessions"
+}
+
   namespace :public do
-    get 'homes/top'
-    get 'homes/about'
+    resources :posts, only: [:index,:show,:create,:edit,:update,:destroy]
+    resources :bookmarks, only: [:index,:destroy,:create]
+    resources :spots, only: [:edit,:update,:destroy]
+    resources :reviews, only: [:index,:create,:update,:destroy]
+    resources :searches, only: [:get]
+    # ここからmembersのルーティング
+      get "members/my_page" => "customers#show", as: "my_page"
+      get "members/my_page/edit" => "customers#edit", as: "edit"
+      patch "members/members" => "customers#update", as: "update"
+       # 退会確認画面
+      get 'members/unsubscribe' => 'customers#unsubscribe', as: 'unsubscribe'
+      # 論理削除用のルーティング
+      patch 'members/withdraw' => 'customers#withdraw', as: 'withdraw'
+    # ここまでmembersのルーティング
+  end
+
+  namespace :admin do
+    get "/home/top" => "homes#top"
+    resources :members, only: [:index,:show,:edit,:update]
+    resources :posts, only: [:index,:show,:destroy]
+    resources :spots, only: [:index,:show,:edit,:update,:destroy]
+    resources :reviews, only: [:index,:destroy]
+    resources :searches, only: [:get]
+
   end
 
 
