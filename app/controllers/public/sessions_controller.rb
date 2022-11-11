db5b8ea62742659877c82910ef3df1ada5259ec7
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-11-11 06:19:27 +0000

    [Add]sessions_controller.rbに退会機能に関する内容を記述

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action  :member_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +19,34 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  def member_state
+    @member=Member.find_by(email: params[:member][:email])
+    return if !@member
+      flash[:notice] = "該当するアカウントが見つかりません"
+    if @member.vaild_passward?(params[:member][:passward]) && (@member.is_active == false)
+      redirect_to new_member_registration_path
+    else
+      flash[:notice] = "項目を入力してください"
+    end
+  end
+
+  def after_sigin_out_path_for(resource)
+    root_path
+  end
+
+  # 会員の論理削除のための記述。退会後のログインを阻止。
+  def reject_user
+    @member=Member.find_by(name: params[:customer][:name])
+    if @customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
+        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
+        redirect_to root_path
+      else
+        flash[:notice] = "項目を入力してください"
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
