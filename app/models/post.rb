c526cdd5ecbdd89323f0f6450e9b26d069811907
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-11-12 01:59:17 +0000

    [Add]各モデルにアソシエーション記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  belongs_to :spot
+  belongs_to :member
 end
--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,3 @@
 class Spot < ApplicationRecord
+  has_many :posts, dependent: :destroy
 end
