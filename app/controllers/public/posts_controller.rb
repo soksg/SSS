68e321972cb4e68d3fdde842a17f33297d5b3094
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-11-13 12:37:47 +0000

    [Fix]post_controller修正中

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -7,7 +7,7 @@ class Public::PostsController < ApplicationController
     @post=Post.new(post_params)
     @post.member_id=current_member.id
     if  @post.save
-        redirect_to spots_path notice: "投稿しました"
+        redirect_to post_path(@post), notice: "投稿しました"
     else
         @post=Post.new
         render "new"
@@ -29,7 +29,7 @@ class Public::PostsController < ApplicationController
   def update
     @post=Post.find(params[:id])
     if  @post.update(post_params)
-        redirect_to posts_path notice: "投稿内容を更新しました"
+        redirect_to posts_path, notice: "投稿内容を更新しました"
     else
         render "edit"
     end
@@ -38,7 +38,7 @@ class Public::PostsController < ApplicationController
   def destroy
     @post=Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path notice: "投稿を削除しました"
+    redirect_to posts_path, notice: "投稿を削除しました"
   end
 
   private
