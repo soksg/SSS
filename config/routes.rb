17c689c2e1463ff97b6c9cc972e884fcdc129e94
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-11-18 01:47:57 +0000

    [Fix]deviseの不具合を修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :admins
-  root to: "public/posts#index"
   get "/home/about" => "homes#about"
 
   devise_for :members, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: "public/sessions"
 }
+
   devise_for :admins, skip: [:passwords, :registrations] ,controllers: {
   sessions: "admin/sessions"
 }
