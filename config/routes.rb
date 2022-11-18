14f5c2992ed2da875509b92ce204a5f483d91495
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-11-18 01:38:11 +0000

    [Fix]devisenoに問題あり

--- config/routes.rb
+++ config/routes.rb
@@ -7,31 +7,28 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
   sessions: "public/sessions"
 }
-  devise_for :admins, skip: [:passwords, :registrations] ,controllers: {
-  sessions: "admin/sessions"
-}
+#   devise_for :admins, skip: [:passwords, :registrations] ,controllers: {
+#   sessions: "admin/sessions"
+# }
 
   scope module: :public do
     resources :posts, only: [:new,:index,:show,:create,:edit,:update,:destroy] do
-      resources :bookmarks, only: [:index,:destroy,:create]
+      resource :bookmarks, only: [:destroy,:create]
       resources :reviews, only: [:index,:create,:update,:destroy]
-      resources :post_comments, only: [:create]
+      resources :post_comments, only: [:create,:destroy]
+    end
+    resources :members, only: [:show, :edit, :update] do
+      member do
+        get :bookmarks
+      end
     end
   end
   # ここからmembersのルーティング
-  get "members/my_page" => "members#show", as: "my_page"
-  get "members/my_page/edit" => "members#edit", as: "edit"
-  patch "members/members" => "members#update", as: "update"
    # 退会確認画面
   get 'members/unsubscribe' => 'members#unsubscribe', as: 'unsubscribe'
-  # 論理削除用のルーティング
+   # 論理削除用のルーティング
   patch 'members/withdraw' => 'members#withdraw', as: 'withdraw'
-  resources :members, only: [:show] do
-    member do
-    get :bookmarks
-  end
 
-end
 
   namespace :admin do
     resources :members, only: [:index,:show,:edit,:update]
@@ -41,11 +38,6 @@ end
     end
     resources :tags, only: [:new, :create, :index, :edit, :destroy]
   end
-
-
-devise_scope :user do
-  post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
-end
 
 end
 
