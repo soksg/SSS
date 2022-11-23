675956397411ed6080a3f9b088b766ae85756460
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-11-23 08:02:03 +0000

    [Fix]管理者ログイン時でもロゴを押すとメンバーの投稿一覧に遷移する問題を修正

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,3 +1,13 @@
 class Admin::TagsController < ApplicationController
   before_action :authenticate_admin!
+
+  def show
+    #検索結果画面でもタグ一覧表示
+    @tag_list=Tag.all
+    #検索されたタグを受け取る
+    @tag=Tag.find(params[:id])
+    @tag.posts
+    #検索されたタグに紐づく投稿を表示
+    @posts=@tag.posts
+  end
 end
