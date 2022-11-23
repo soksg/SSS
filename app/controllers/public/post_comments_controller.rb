f27be606c033637c3b33b19d3f954bd296073ec7
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-11-23 11:44:07 +0000

    [Fix]コメント非同期通信・検索窓修正

--- app/controllers/public/post_comments_controller.rb
+++ app/controllers/public/post_comments_controller.rb
@@ -5,17 +5,23 @@ class Public::PostCommentsController < ApplicationController
     @post_comment = current_member.post_comments.new(post_comment_params)
     @post_comment.post_id = @post.id
     if @post_comment.save
-      flash[:notice] = 'レビューを投稿しました'
+      flash.now[:notice] = 'レビューを投稿しました'
+      #非同期化のため、追記
+      @post_comment_new=PostComment.new
+      render "public/comments/create"
     else
       render post_path(@post)
-      flash[:alert] = 'レビューの投稿に失敗しました'
+      flash.now[:alert] = 'レビューの投稿に失敗しました'
     end
   end
 
   def destroy
     PostComment.find(params[:id]).destroy
-    flash[:alert] = 'レビューを削除しました'
+    flash.now[:alert] = 'レビューを削除しました'
     @post = Post.find(params[:post_id])
+    #destroy.jsで使う変数を定義する
+    @post_comment=PostComment.new
+    render "public/comments/destroy"
   end
 
   private
