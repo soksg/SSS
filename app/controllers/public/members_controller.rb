b08dd8a6802e13c3001fc98bbe143236624d6bb1
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-11-24 07:13:07 +0000

    [Fix]correctのメソッドの記述を修正

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -3,50 +3,49 @@ class Public::MembersController < ApplicationController
   before_action :correct_member, only: [:show, :edit, :bookmarks]
   before_action :ensure_guest_member, only: [:edit, :withdraw]
 
-    def show
-      @member=Member.find(params[:id])
-      @posts=@member.posts
-      @tag_list=Tag.all
-    end
+  def show
+    @member=Member.find(params[:id])
+    @posts=@member.posts
+    @tag_list=Tag.all
+  end
 
-    def edit
-      @member=Member.find(params[:id])
-    end
+  def edit
+    @member=Member.find(params[:id])
+  end
 
-    def update
-      @member=Member.find(params[:id])
-      if @member.update(member_params)
-         redirect_to member_path(@member), notice: "プロフィールを更新しました"
-      else
-         render 'edit', alert: "項目を入力してください"
-      end
-
+  def update
+    @member=Member.find(params[:id])
+    if @member.update(member_params)
+       redirect_to member_path(@member), notice: "プロフィールを更新しました"
+    else
+       render 'edit', alert: "項目を入力してください"
     end
+  end
 
-    def bookmarks
-      @member=Member.find(params[:id])
-      bookmarks=Bookmark.where(member_id: @member.id).pluck(:post_id)
-      @bookmark_posts=Post.find(bookmarks)
-      @tag_list=Tag.all
-    end
+  def bookmarks
+    @member=Member.find(params[:id])
+    bookmarks=Bookmark.where(member_id: @member.id).pluck(:post_id)
+    @bookmark_posts=Post.find(bookmarks)
+    @tag_list=Tag.all
+  end
 
-    def guest_sign_in
-      member = Member.guest
-      sign_in member
-      redirect_to member_path(member), notice: 'ゲストメンバーでログインしました。'
-    end
+  def guest_sign_in
+    member = Member.guest
+    sign_in member
+    redirect_to member_path(member), notice: 'ゲストメンバーでログインしました。'
+  end
 
-    def unsubscribe
-    end
+  def unsubscribe
+  end
 
-    def withdraw
-      @member = current_member
-      # is_activeカラムをfalseに変更することにより削除フラグを立てる
-      @member.update(is_active: false)
-      reset_session
-      flash[:notice] = "退会処理を実行いたしました"
-      redirect_to root_path
-    end
+  def withdraw
+    @member = current_member
+    # is_activeカラムをfalseに変更することにより削除フラグを立てる
+    @member.update(is_active: false)
+    reset_session
+    flash[:notice] = "退会処理を実行いたしました"
+    redirect_to root_path
+  end
 
   private
   def member_params
@@ -60,7 +59,7 @@ class Public::MembersController < ApplicationController
     end
   end
 
-  def correnct_member
+  def correct_member
     @member=Member.find(params[:id])
     unless @member.id == current_member.id
        redirect_to posts_path, alert: '他ユーザーのページは見れません'
