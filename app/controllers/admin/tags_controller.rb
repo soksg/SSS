d73def5072bb2f889d2285231796fb58fd974312
EC2 Default User <ec2-user@ip-172-31-10-166.ap-northeast-1.compute.internal>
2022-12-20 10:40:54 +0000

    [Fix]インデント修正

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -3,9 +3,9 @@ class Admin::TagsController < ApplicationController
 
   def show
     #検索結果画面でもタグ一覧表示
-    @tag_list=Tag.all
+    @tag_list = Tag.all
     #検索されたタグを受け取る
-    @tag=Tag.find(params[:id])
+    @tag = Tag.find(params[:id])
     @tag.posts
     #検索されたタグに紐づく投稿を表示
     @posts=@tag.posts.is_active.page(params[:page]).per(7)
